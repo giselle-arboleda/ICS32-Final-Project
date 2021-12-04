ebc762bf744b789354406ce65047a3552d763de2
giselle-arboleda <91309234+giselle-arboleda@users.noreply.github.com>
2021-12-04 11:02:33 -0800

    Add files via upload

---
+++ test_ds_message_protocol.py
@@ -0,0 +1,24 @@
+#test_ds_message_protocol.py
+
+from ds_protocol import directmessage
+import unittest
+import json
+
+
+
+class CommandTest(unittest.TestCase):
+    dm_msg = {"entry": "Hello World!","recipient":"ohhimark", "timestamp": "1603167689.3928561"}
+    dm_new = "new"
+    dm_all = "all"
+    user_token = "user_token"
+    
+    def test_directmessage(self):
+        self.assertEqual(directmessage(self.user_token, self.dm_msg), json.dumps({"token":"user_token", "directmessage": {"entry": "Hello World!","recipient":"ohhimark", "timestamp": "1603167689.3928561"}}))
+        self.assertEqual(directmessage(self.user_token, self.dm_new), json.dumps({"token":"user_token", "directmessage": "new"}))
+        self.assertEqual(directmessage(self.user_token, self.dm_all), json.dumps({"token":"user_token", "directmessage": "all"}))
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
