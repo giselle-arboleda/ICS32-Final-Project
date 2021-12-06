0663fdd4657647a44469d61dbf538cbc6e61948a
giselle-arboleda <91309234+giselle-arboleda@users.noreply.github.com>
2021-12-05 17:39:53 -0800

    Add files via upload

---
+++ test_ds_messenger.py
@@ -0,0 +1,23 @@
+#test_ds_messenger.py
+
+from ds_messenger import DirectMessage, DirectMessenger
+import unittest
+
+class DSMessengerTest(unittest.TestCase):
+
+    def test_dmsend(self):
+        dm = DirectMessenger("168.235.86.101", "EvanKarGiselle", "pwd123")
+        self.assertTrue(dm.send('testing message entry 1', 'ohhimark'))
+        self.assertTrue(dm.send('testing message entry 2', 'EvanKarGiselle'))
+
+    def test_retrieve_new(self):
+        dm = DirectMessenger("168.235.86.101", "EvanKarGiselle", "pwd123")
+        self.assertIn({'message': 'testing message entry 2', 'from': 'EvanKarGiselle', 'timestamp': '1638652218.10286'}, dm.retrieve_new())
+
+    def test_retrieve_all(self):
+        dm = DirectMessenger("168.235.86.101", "EvanKarGiselle", "pwd123")
+        self.assertIn({'message': 'testing message entry 2', 'from': 'EvanKarGiselle', 'timestamp': '1638652218.10286'}, dm.retrieve_all())
+
+
+if __name__ == '__main__':
+    unittest.main()
